ed649d3e1b6ba372031021c2927a8c724ee97acf
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-18 22:25:05 +0530

    Spring boot OpenAPI3

--- Spring Boot/Rest-Endpoint-Documentation-SwaggerAPI3/src/main/java/com/myLearning/beanValidation/exceptionHandling.documentation/controller/CourseController.java
+++ Spring Boot/Rest-Endpoint-Documentation-SwaggerAPI3/src/main/java/com/myLearning/beanValidation/exceptionHandling.documentation/controller/CourseController.java
@@ -6,6 +6,7 @@ import com.myLearning.beanValidation.exceptionHandling.documentation.dto.Service
 import com.myLearning.beanValidation.exceptionHandling.documentation.service.CourseService;
 import com.myLearning.beanValidation.exceptionHandling.documentation.utils.AppUtils;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class CourseController {
 
     // http://localhost:8084/swagger-ui.html
@@ -22,62 +24,60 @@ public class CourseController {
 
     private final CourseService courseService;
 
-    Logger logger = LoggerFactory.getLogger(CourseController.class);
-
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
     }
 
     @PostMapping("addCourse")
     public ServiceResponse<CourseResponseDto> addCourse(@RequestBody @Valid CourseRequestDto courseDto) {
-        logger.info("Inside CourseController:addRequest api");
-        logger.info("CourseController:addRequest Request Payload -> {} ", AppUtils.getCourseFromDto(courseDto));
+        log.info("Inside CourseController:addRequest api");
+        log.info("CourseController:addRequest Request Payload -> {} ", AppUtils.getCourseFromDto(courseDto));
         ServiceResponse<CourseResponseDto> serviceResponse = new ServiceResponse<>();
         CourseResponseDto employeeResponseDto = courseService.addCourse(courseDto);
         serviceResponse.setResponse(employeeResponseDto);
         serviceResponse.setStatus(HttpStatus.CREATED);
-        logger.info("Exiting CourseController:addRequest api");
-        logger.info("CourseController:addRequest Response -> {} ", serviceResponse.toString());
+        log.info("Exiting CourseController:addRequest api");
+        log.info("CourseController:addRequest Response -> {} ", serviceResponse.toString());
         return serviceResponse;
     }
 
     @GetMapping("getAllCourseByIds")
     public ServiceResponse<List<CourseResponseDto>> getAllCourses(@RequestParam("courseIdList") Iterable<Integer> courseIds) {
-        logger.info("Inside CourseController:getAllCourses api");
-        logger.info("CourseController:getAllCourses Request Payload -> {} ", courseIds.toString());
+        log.info("Inside CourseController:getAllCourses api");
+        log.info("CourseController:getAllCourses Request Payload -> {} ", courseIds.toString());
         ServiceResponse<List<CourseResponseDto>> serviceResponse = new ServiceResponse<>();
         List<CourseResponseDto> courseResponseDtoList = courseService.getAllCourseById(courseIds);
         serviceResponse.setResponse(courseResponseDtoList);
         serviceResponse.setStatus(HttpStatus.CREATED);
-        logger.info("Exiting CourseController:getAllCourses api");
+        log.info("Exiting CourseController:getAllCourses api");
         return serviceResponse;
     }
 
     @GetMapping("getCourseById")
     public ServiceResponse<CourseResponseDto> getCourseById(@RequestParam("courseId") Integer id) {
-        logger.info("Inside CourseController:getCourseById api");
+        log.info("Inside CourseController:getCourseById api");
         ServiceResponse<CourseResponseDto> serviceResponse = new ServiceResponse<>();
         CourseResponseDto courseResponseDto = courseService.getCourseById(id);
         serviceResponse.setResponse(courseResponseDto);
         serviceResponse.setStatus(HttpStatus.CREATED);
-        logger.info("Exiting CourseController:getCourseById api");
+        log.info("Exiting CourseController:getCourseById api");
         return serviceResponse;
     }
 
     @PutMapping("updateCourse/{courseId}")
     public ServiceResponse<CourseResponseDto> updateCourse(@PathVariable("courseId") Integer courseId, @RequestBody @Valid CourseRequestDto courseDto) {
-        logger.info("Inside CourseController:updateCourse api");
+        log.info("Inside CourseController:updateCourse api");
         ServiceResponse<CourseResponseDto> serviceResponse = new ServiceResponse<>();
         CourseResponseDto courseResponseDto = courseService.updateCourse(courseId, courseDto);
         serviceResponse.setResponse(courseResponseDto);
         serviceResponse.setStatus(HttpStatus.OK);
-        logger.info("Exiting CourseController:updateCourse api");
+        log.info("Exiting CourseController:updateCourse api");
         return serviceResponse;
     }
 
     @DeleteMapping("deleteCourse")
     public ServiceResponse<?> deleteCourse(@RequestParam("courseId") Integer id) {
-        logger.info("Inside CourseController:deleteCourse api");
+        log.info("Inside CourseController:deleteCourse api");
         return new ServiceResponse<>(courseService.deleteCourseById(id), HttpStatus.OK);
     }
 }
