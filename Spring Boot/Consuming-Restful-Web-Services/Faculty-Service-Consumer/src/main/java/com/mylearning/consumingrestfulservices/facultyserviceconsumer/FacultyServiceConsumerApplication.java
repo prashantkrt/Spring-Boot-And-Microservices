b213ace1292c0b81536c546971edbcb58644597d
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-20 22:23:40 +0530

    Spring Boot

---
+++ Spring Boot/Consuming-Restful-Web-Services/Faculty-Service-Consumer/src/main/java/com/mylearning/consumingrestfulservices/facultyserviceconsumer/FacultyServiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.consumingrestfulservices.facultyserviceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyServiceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacultyServiceConsumerApplication.class, args);
+    }
+
+}
---
+++ Spring Boot/Consuming-Restful-Web-Services/Faculty-Service-Consumer/src/test/java/com/mylearning/consumingrestfulservices/facultyserviceconsumer/FacultyServiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.consumingrestfulservices.facultyserviceconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyServiceConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
