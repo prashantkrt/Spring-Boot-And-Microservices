c4ae0f16c18fdd88b8cae7978e16a93e25a270e3
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-21 21:06:32 +0530

    Spring Boot Actuator

---
+++ Spring Boot/Spring-Boot-Actuator/src/main/java/com/mylearning/actuator/SpringBootActuatorApplication.java
@@ -0,0 +1,14 @@
+package com.mylearning.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorApplication {
+
+	//comment
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/Spring-Boot-Actuator/src/test/java/com/mylearning/actuator/SpringBootActautorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActautorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
