c850ae1f88fac87448c073420f06a798ccd0345e
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-21 21:51:38 +0530

    spring boot actuator

--- Spring Boot/Spring-Boot-Actuator/src/main/java/com/mylearning/actuator/controller/EmployeeController.java
+++ Spring Boot/Spring-Boot-Actuator/src/main/java/com/mylearning/actuator/controller/EmployeeController.java
@@ -1,4 +1,31 @@
 package com.mylearning.actuator.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mylearning.actuator.dto.Employee;
+import com.mylearning.actuator.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() throws JsonProcessingException {
+        List<Employee> employees = employeeService.getEmployees();
+        log.info("EmployeeController:getAllEmployees fetch all employees {}", new ObjectMapper().writeValueAsString(employees));
+        return employees;
+    }
+
+
+
 }
