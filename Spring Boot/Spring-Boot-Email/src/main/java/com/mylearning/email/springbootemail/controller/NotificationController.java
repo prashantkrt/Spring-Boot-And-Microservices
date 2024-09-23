de08fd5c98b2812ea6353660329d943d36d88897
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-23 21:44:39 +0530

    spring boot email

--- Spring Boot/Spring-Boot-Email/src/main/java/com/mylearning/email/springbootemail/controller/NotificationController.java
+++ Spring Boot/Spring-Boot-Email/src/main/java/com/mylearning/email/springbootemail/controller/NotificationController.java
@@ -1,10 +1,29 @@
 package com.mylearning.email.springbootemail.controller;
 
 
+import com.mylearning.email.springbootemail.dto.EmailRequest;
+import com.mylearning.email.springbootemail.service.EmailService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class NotificationController {
+
+    @Autowired
+    private EmailService service;
+
+    @PostMapping("/sendEmail")
+    public String sendTextEmail(@RequestBody EmailRequest request) {
+        return service.sendSimpleEmail(request);
+    }
+
+    @PostMapping("/sendAttachment")
+    public String sendEmailWithAttachment(@RequestBody EmailRequest request) throws MessagingException {
+        return service.sendEmailWithAttachment(request);
+    }
 }
--- Spring Boot/Spring-Boot-Email/src/main/java/com/mylearning/email/springbootemail/service/EmailService.java
+++ Spring Boot/Spring-Boot-Email/src/main/java/com/mylearning/email/springbootemail/service/EmailService.java
@@ -1,4 +1,56 @@
 package com.mylearning.email.springbootemail.service;
 
+
+import com.mylearning.email.springbootemail.dto.EmailRequest;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Objects;
+
+@Service
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+
+    public String sendSimpleEmail(EmailRequest request) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(sender);
+        mailMessage.setTo(request.getToEMail());
+        mailMessage.setTo(request.getToEmails());
+        mailMessage.setSubject(request.getSubject());
+        mailMessage.setText(request.getMessageBody());
+        javaMailSender.send(mailMessage);
+        return "email successfully send to : " + request.getToEMail();
+    }
+
+    public String sendEmailWithAttachment(EmailRequest request) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setFrom(sender);
+        helper.setTo(request.getToEmails());
+        helper.setSubject(request.getSubject());
+        helper.setText(request.getMessageBody());
+
+        FileSystemResource file = new FileSystemResource(new File(request.getAttachment()));
+        //helper.addAttachment(file.getFilename(), file);
+        helper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+        javaMailSender.send(mimeMessage);
+        return "Mail sent successfully with attachment " + file.getFilename();
+    }
+
 }
