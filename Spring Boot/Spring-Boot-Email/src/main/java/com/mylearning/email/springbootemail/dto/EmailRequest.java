8f903d445ab469ef62e29171b9a10a71b60732e0
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-24 22:09:50 +0530

    spring boot email

--- Spring Boot/Spring-Boot-Email/src/main/java/com/mylearning/email/springbootemail/dto/EmailRequest.java
+++ Spring Boot/Spring-Boot-Email/src/main/java/com/mylearning/email/springbootemail/dto/EmailRequest.java
@@ -13,5 +13,29 @@ public class EmailRequest {
     private String subject;
     private String messageBody;
     private String attachment;
-    private String[] toEmails;
+    private String[] toEmails; // this value will be given through the postman api
 }
+
+//{
+//        "toEMail":"yourMail@gmail.com",
+//        "subject":"Testing my application",
+//        "messageBody": "<b>Hello</b> This is just a sample message which is sent to you as a test result!!  \n\n Please ignore the same ",
+//        "attachment":"No",
+//        "toEmails": [
+//        "yourMail1@gmail.com",
+//        "yourMail2@gmail.com"
+//        ]
+//}
+
+
+// with attachment
+//{
+//        "toEMail":"youremail@gmail.com",
+//        "subject":"Testing my application",
+//        "messageBody": "Hello This is just sample message which is been sent to you as a test result Please ignore the same ",
+//        "attachment":"/path/fileName.pdf",
+//        "toEmails":[
+//        "youremail1@gmail.com",
+//        "youremail2@gmail.com"
+//        ]
+//        }
--- Spring Boot/Spring-Boot-Email/src/main/java/com/mylearning/email/springbootemail/service/EmailService.java
+++ Spring Boot/Spring-Boot-Email/src/main/java/com/mylearning/email/springbootemail/service/EmailService.java
@@ -1,6 +1,5 @@
 package com.mylearning.email.springbootemail.service;
 
-
 import com.mylearning.email.springbootemail.dto.EmailRequest;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
@@ -24,31 +23,34 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String sender;
 
-
     public String sendSimpleEmail(EmailRequest request) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setFrom(sender);
-        mailMessage.setTo(request.getToEMail());
-        mailMessage.setTo(request.getToEmails());
+        mailMessage.setTo(request.getToEMail()); // we can pass single String
+        mailMessage.setTo(request.getToEmails());  // we can pass String…(String args)
         mailMessage.setSubject(request.getSubject());
         mailMessage.setText(request.getMessageBody());
+        mailMessage.setCc(); // we can pass single String
+        mailMessage.setCc();  // we can pass String…(String args)
+        mailMessage.setBcc();  // we can pass String…(String args)
+        mailMessage.setBcc();  // we can pass String…(String args)
         javaMailSender.send(mailMessage);
         return "email successfully send to : " + request.getToEMail();
     }
 
     public String sendEmailWithAttachment(EmailRequest request) throws MessagingException {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-
         helper.setFrom(sender);
-        helper.setTo(request.getToEmails());
+        helper.setTo(request.getToEMail()); // single mail
+        helper.setTo(request.getToEmails()); // multiple mail
         helper.setSubject(request.getSubject());
         helper.setText(request.getMessageBody());
-
+//        helper.setCc();
+//        helper.setBcc();
         FileSystemResource file = new FileSystemResource(new File(request.getAttachment()));
-        //helper.addAttachment(file.getFilename(), file);
-        helper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+        helper.addAttachment(file.getFilename(), file);
+//        helper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
         javaMailSender.send(mimeMessage);
         return "Mail sent successfully with attachment " + file.getFilename();
     }
