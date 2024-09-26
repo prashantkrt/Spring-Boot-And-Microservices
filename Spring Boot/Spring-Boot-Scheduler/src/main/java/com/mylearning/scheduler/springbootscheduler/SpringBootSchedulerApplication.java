75f2f762d4e15fb3157f9db08cfcc78c35da40cc
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-26 20:51:27 +0530

    spring boot Scheduler

---
+++ Spring Boot/Spring-Boot-Scheduler/src/main/java/com/mylearning/scheduler/springbootscheduler/SpringBootSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.scheduler.springbootscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSchedulerApplication.class, args);
+    }
+
+}
---
+++ Spring Boot/Spring-Boot-Scheduler/src/test/java/com/mylearning/scheduler/springbootscheduler/SpringBootSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.scheduler.springbootscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
