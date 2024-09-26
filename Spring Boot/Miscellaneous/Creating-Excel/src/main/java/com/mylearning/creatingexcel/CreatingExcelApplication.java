e6387a3d99e67525c5f86a1feb9125f133ca6254
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-26 22:19:38 +0530

    Excel Creation using Apache POI

---
+++ Spring Boot/Miscellaneous/Creating-Excel/src/main/java/com/mylearning/creatingexcel/CreatingExcelApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.creatingexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingExcelApplication.class, args);
+    }
+
+}
---
+++ Spring Boot/Miscellaneous/Creating-Excel/src/test/java/com/mylearning/creatingexcel/CreatingExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.creatingexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
