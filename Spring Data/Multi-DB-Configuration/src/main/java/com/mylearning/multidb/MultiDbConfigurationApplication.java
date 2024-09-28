7d9ef8151a4efb23b5d900f53bb44d85f9efec44
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-28 19:16:29 +0530

    Spring boot Multi DB Config

---
+++ Spring Data/Multi-DB-Configuration/src/main/java/com/mylearning/multidb/MultiDbConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.multidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDbConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiDbConfigurationApplication.class, args);
+    }
+
+}
---
+++ Spring Data/Multi-DB-Configuration/src/test/java/com/mylearning/multidb/MultiDbConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.multidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDbConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
