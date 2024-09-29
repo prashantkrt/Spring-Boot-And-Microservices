8a4ede8fefcd764e127f562bcf04b5b185b4d245
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-29 13:22:20 +0530

    Spring boot Messaging

---
+++ Spring Boot Messaging/Kafka-Producer/src/main/java/com/mylearning/produce/controller/PaytmController.java
@@ -0,0 +1,27 @@
+package com.mylearning.produce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PaytmController {
+
+    @Value("${paytm.producer.topic.name}")
+    private String topicName;
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @RequestMapping(value = "/publish/{message}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void sendMessage(@PathVariable("message") String message) {
+        kafkaTemplate.send(topicName, message);
+    }
+
+}
