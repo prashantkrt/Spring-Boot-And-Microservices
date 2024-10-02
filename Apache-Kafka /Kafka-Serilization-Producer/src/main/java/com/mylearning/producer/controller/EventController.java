5e788a3738059b74e6fe2447624d69befc934d82
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-02 23:06:34 +0530

    Apache Kafka Serialization

---
+++ Apache-Kafka /Kafka-Serilization-Producer/src/main/java/com/mylearning/producer/controller/EventController.java
@@ -0,0 +1,42 @@
+package com.mylearning.producer.controller;
+
+
+import com.mylearning.producer.dto.Customer;
+import com.mylearning.producer.service.KafkaMessagePublisher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher kafkaMessagePublisher;
+
+    @PostMapping("/publish")
+    public ResponseEntity<?> publishMessage(@RequestBody Customer customer) {
+        try {
+            // writing bulk messages for multiple partition distribution
+            for (int i = 0; i < 5000; i++) {
+                customer.setId(i);
+                customer.setName("customer"+i);
+                customer.setEmail("customer"+ UUID.randomUUID().toString().split("-")[0] +"@gmlii.com");
+                long tenDigitNumber = 1000000000L + (long)(new Random().nextDouble() * 9000000000L);
+                customer.setContactName(String.valueOf(tenDigitNumber));
+                kafkaMessagePublisher.sendMessage(customer);
+                log.info("Messages produced -> {}", customer.toString() + i);
+            }
+            return ResponseEntity.ok("message published successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+}
---
+++ Apache-Kafka /Kafka-Serilization-Producer/src/main/java/com/mylearning/producer/service/KafkaMessagePublisher.java
@@ -0,0 +1,42 @@
+package com.mylearning.producer.service;
+
+
+import com.mylearning.producer.dto.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class KafkaMessagePublisher {
+    /*
+     * for String as key as well as Object we don't need to use serializer props in app.props file
+     * for JSON we need to else it will give error
+     *
+     * Here we are using Object as parameter then we have to use serialization
+     *
+     * */
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMessage(Customer customer) throws ExecutionException, InterruptedException {
+
+        // it has a return type in CompletableFuture
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("myTopicDemo2", customer.toString());
+        future.whenComplete((result, exception) -> {
+
+            System.out.println("Partition number: -> " + result.getRecordMetadata().partition()); // prints the partition
+            System.out.println("Offset number: -> " + result.getRecordMetadata().offset()); // prints the offset number
+            System.out.println("Topic name: ->" + result.getRecordMetadata().topic()); // prints the topic
+
+            if (exception == null) {
+                System.out.println("Sent message: = [" + customer.toString() + "]" + " [" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message = [" + customer.toString() + "] due to : " + exception.getMessage());
+            }
+        });
+    }
+}
