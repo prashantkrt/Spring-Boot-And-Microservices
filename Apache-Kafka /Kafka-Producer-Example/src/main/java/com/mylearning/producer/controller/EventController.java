2ad849705c30388b115738554c28619fa98cf743
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-02 20:47:41 +0530

    Apache Kafka Producer

---
+++ Apache-Kafka /Kafka-Producer-Example/src/main/java/com/mylearning/producer/controller/EventController.java
@@ -0,0 +1,30 @@
+package com.mylearning.producer.controller;
+
+
+import com.mylearning.producer.service.KafkaMessagePublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher kafkaMessagePublisher;
+
+    @GetMapping("/publish/{message}")
+    public ResponseEntity<?> publishMessage(@PathVariable String message) {
+        try {
+            kafkaMessagePublisher.sendMessage(message);
+            return ResponseEntity.ok("message published successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+}
---
+++ Apache-Kafka /Kafka-Producer-Example/src/main/java/com/mylearning/producer/service/KafkaMessagePublisher.java
@@ -0,0 +1,34 @@
+package com.mylearning.producer.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class KafkaMessagePublisher {
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMessage(String message) throws ExecutionException, InterruptedException {
+
+        // it has a return type in CompletableFuture
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("myLearning", message);
+        future.whenComplete((result, exception) -> {
+
+            System.out.println(result.getRecordMetadata().partition()); // prints the partition
+            System.out.println(result.getRecordMetadata().offset()); // prints the offset number
+            System.out.println(result.getRecordMetadata().topic()); // prints the topic
+
+            if (exception == null) {
+                System.out.println("Sent message: = [ " + message + "]" + " [ =" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message = [ " + message + " ] due to : " + exception.getMessage());
+            }
+        });
+    }
+}
