a00c620a1b751edf8c32160c98828b688f37c36a
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-03 12:39:09 +0530

    Apache Kafka Serialization/Deserialization

--- Apache-Kafka /Kafka-Consumer-Example/src/main/java/com/mylearning/consumer/KafkaMessageListener.java
+++ Apache-Kafka /Kafka-Consumer-Example/src/main/java/com/mylearning/consumer/KafkaMessageListener.java
@@ -3,6 +3,8 @@ package com.mylearning.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +16,10 @@ public class KafkaMessageListener {
         log.info("Consumer 1 consumed the message -> {}", message+1);
     }
 
-    @KafkaListener(topics = "myTopicDemo", groupId = "DemoGroup-1")
+    // specific partition
+    @KafkaListener(topics = "myTopicDemo", groupId = "DemoGroup-1",topicPartitions = {
+            @TopicPartition(topic = "myTopicDemo",partitions = {"2"}, partitionOffsets = @PartitionOffset(partition = "2", initialOffset = "0")) // Start from offset 0 for partition 2)
+    })
     public void consume2(String message) {
         log.info("Consumer 2 consumed the message -> {}", message+2);
     }
