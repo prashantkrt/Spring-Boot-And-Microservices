b0f6ede03948df51527ee398c946095fab32473d
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-05 17:05:32 +0530

    Apache Kafka Avro Schema

---
+++ Apache-Kafka /Kafka-Schema-Registry/Kafka-Schema-Registry/src/main/java/com/mylearning/registry/consumer/KafkaAvroConsumer.java
@@ -0,0 +1,19 @@
+package com.mylearning.registry.consumer;
+
+import com.mylearning.registry.dto.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaAvroConsumer {
+
+    @KafkaListener(topics = "${topic.name}")
+    public void read(ConsumerRecord<String, Employee> consumerRecord) {
+        String key = consumerRecord.key();
+        Employee employee = consumerRecord.value();
+        log.info("Avro message received for key : " + key + " value : " + employee.toString());
+    }
+}
