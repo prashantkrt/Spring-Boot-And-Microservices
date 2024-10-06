5bd558aefd05211490cd63f8762c7ca396d04988
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-06 11:14:28 +0530

    Spring boot Security

---
+++ Spring Boot Security/Employee-Management-System-Traditional-Approach/src/test/java/com/myLearning/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
