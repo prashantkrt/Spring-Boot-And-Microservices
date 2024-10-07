3942f7e98423f3e406b788f418f09dc773a0a6e1
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-07 12:15:10 +0530

    Spring boot Security

---
+++ Spring Boot Security/Employee-Management-System-Traditional-Approach/src/main/java/com/myLearning/controller/SecurityDemoController.java
@@ -0,0 +1,25 @@
+package com.myLearning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SecurityDemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello-world from Spring Boot Security Application :)";
+    }
+
+    @GetMapping("/greet")
+    public String greetings() {
+        return "Hello greetings from Spring Boot Security Application :)";
+    }
+
+    @GetMapping("/nonSecure")
+    public String nonSecure() {
+        return "Hello this RestEnd point is publicly Available ";
+    }
+}
