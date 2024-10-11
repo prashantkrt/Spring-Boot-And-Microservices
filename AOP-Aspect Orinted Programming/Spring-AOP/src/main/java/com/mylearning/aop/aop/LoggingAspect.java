613ce62b8a48deffdd0698ac710d4a4869bb4d9a
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-11 23:17:56 +0530

    Spring boot AOP After

--- AOP-Aspect Orinted Programming/Spring-AOP/src/main/java/com/mylearning/aop/aop/LoggingAspect.java
+++ AOP-Aspect Orinted Programming/Spring-AOP/src/main/java/com/mylearning/aop/aop/LoggingAspect.java
@@ -1,7 +1,8 @@
 package com.mylearning.aop.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.After;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -12,8 +13,35 @@ public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution (* com.mylearning.aop.service.JobService.getJob(..))")
-    public void logMethodCall() {
+    // return Type , class name , method name , args
+    // PointCut -> (* *.*.(..))  all return type , all classes , all methods and all args
+
+    @Before("execution (* com.mylearning.aop.service.JobService.getJob(..)) || execution (* com.mylearning.aop.service.JobService.getAllJobs(..))")
+    public void logMethodCall(JoinPoint joinpoint) {
+
        logger.info("Executing method call before execution of getJob(..) method");
+       //JoinPoint to get the hold on !!!
+
+
+       logger.info(joinpoint.getSignature().getName()); //getJob
+        // com.mylearning.aop.service.JobService + getJob
+       logger.info(joinpoint.getSignature().getDeclaringTypeName() + "." + joinpoint.getSignature().getName());
     }
+
+    // when we use After it act as After Finally it will execute even some issue comes
+    @After("execution (* com.mylearning.aop.service.JobService.getJob(..)) || execution(* com.mylearning.aop.service.JobService.updateJob(..))")
+    public void logMethodExecuted(JoinPoint jp) {
+        logger.info("Method Executed "+jp.getSignature().getName());
+    }
+
+    @AfterThrowing("execution (* com.mylearning.aop.service.JobService.getJob(..))) || execution(* com.mylearning.aop.service.JobService.updateJob(..))")
+    public void logMethodCrashed(JoinPoint jp) {
+        logger.info("Method has some issues "+jp.getSignature().getName());
+    }
+
+    @AfterReturning("execution (* com.mylearning.aop.service.JobService.getJob(..))) || execution(* com.mylearning.aop.service.JobService.updateJob(..))")
+    public void logMethodExecutedSuccess(JoinPoint jp) {
+        logger.info("Method Executed Successfully "+jp.getSignature().getName());
+    }
+
 }
