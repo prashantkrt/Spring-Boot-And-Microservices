426fb82010ce5e4ebef1a2fc4da265c7f7601f64
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-11 22:54:30 +0530

    Spring boot AOP Before

--- AOP-Aspect Orinted Programming/Spring-AOP/src/main/java/com/mylearning/aop/aop/LoggingAspect.java
+++ AOP-Aspect Orinted Programming/Spring-AOP/src/main/java/com/mylearning/aop/aop/LoggingAspect.java
@@ -1,13 +1,19 @@
 package com.mylearning.aop.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
+    @Before("execution (* com.mylearning.aop.service.JobService.getJob(..))")
     public void logMethodCall() {
-        System.out.println("Logging MethodCall");
+       logger.info("Executing method call before execution of getJob(..) method");
     }
 }
