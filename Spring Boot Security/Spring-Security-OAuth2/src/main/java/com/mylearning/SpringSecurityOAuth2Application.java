ed41ac3458772b197ed2e14fa56e6c07bc5e67ad
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-11 19:35:31 +0530

    Spring boot Security

---
+++ Spring Boot Security/Spring-Security-OAuth2/src/main/java/com/mylearning/SpringSecurityOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.mylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOAuth2Application.class, args);
+    }
+
+}
