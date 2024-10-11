0d18813eaf68ba765dbab2721eda37aec50c9494
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-11 23:39:32 +0530

    Spring boot AOP Around

--- AOP-Aspect Orinted Programming/Spring-AOP/src/main/java/com/mylearning/aop/aop/LoggingAspect.java
+++ AOP-Aspect Orinted Programming/Spring-AOP/src/main/java/com/mylearning/aop/aop/LoggingAspect.java
@@ -1,8 +1,8 @@
 package com.mylearning.aop.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.aspectj.lang.annotation.After;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,29 +19,39 @@ public class LoggingAspect {
     @Before("execution (* com.mylearning.aop.service.JobService.getJob(..)) || execution (* com.mylearning.aop.service.JobService.getAllJobs(..))")
     public void logMethodCall(JoinPoint joinpoint) {
 
-       logger.info("Executing method call before execution of getJob(..) method");
-       //JoinPoint to get the hold on !!!
+        logger.info("Executing method call before execution of getJob(..) method");
+        //JoinPoint to get the hold on !!!
 
 
-       logger.info(joinpoint.getSignature().getName()); //getJob
+        logger.info(joinpoint.getSignature().getName()); //getJob
         // com.mylearning.aop.service.JobService + getJob
-       logger.info(joinpoint.getSignature().getDeclaringTypeName() + "." + joinpoint.getSignature().getName());
+        logger.info(joinpoint.getSignature().getDeclaringTypeName() + "." + joinpoint.getSignature().getName());
     }
 
     // when we use After it act as After Finally it will execute even some issue comes
     @After("execution (* com.mylearning.aop.service.JobService.getJob(..)) || execution(* com.mylearning.aop.service.JobService.updateJob(..))")
     public void logMethodExecuted(JoinPoint jp) {
-        logger.info("Method Executed "+jp.getSignature().getName());
+        logger.info("Method Executed " + jp.getSignature().getName());
     }
 
     @AfterThrowing("execution (* com.mylearning.aop.service.JobService.getJob(..))) || execution(* com.mylearning.aop.service.JobService.updateJob(..))")
     public void logMethodCrashed(JoinPoint jp) {
-        logger.info("Method has some issues "+jp.getSignature().getName());
+        logger.info("Method has some issues " + jp.getSignature().getName());
     }
 
     @AfterReturning("execution (* com.mylearning.aop.service.JobService.getJob(..))) || execution(* com.mylearning.aop.service.JobService.updateJob(..))")
     public void logMethodExecutedSuccess(JoinPoint jp) {
-        logger.info("Method Executed Successfully "+jp.getSignature().getName());
+        logger.info("Method Executed Successfully " + jp.getSignature().getName());
     }
 
+    //Around =>  start and end both the end
+    //when we use Around we should be return the object else method won't work
+    @Around("execution (* com.mylearning.aop.service.JobService.getJob(..))")
+    public Object timeToExecute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object object = joinPoint.proceed();// now start the method
+        long endTime = System.currentTimeMillis();
+        logger.info("Time Taken ->" + (endTime - startTime) + " ms");
+        return object;
+    }
 }
