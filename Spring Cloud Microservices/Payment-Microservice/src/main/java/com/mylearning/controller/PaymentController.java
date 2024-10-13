1bef42310593d145eecb7db8605b3b11d3844d21
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-13 22:54:14 +0530

    Microservice

--- Spring Cloud Microservices/Payment-Microservice/src/main/java/com/mylearning/controller/PaymentController.java
+++ Spring Cloud Microservices/Payment-Microservice/src/main/java/com/mylearning/controller/PaymentController.java
@@ -1,10 +1,23 @@
 package com.mylearning.controller;
 
+import com.mylearning.entity.Payment;
+import com.mylearning.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/payments")
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping("/{orderId}")
+    public Payment getPayment(@PathVariable String orderId) {
+        return paymentService.getByOrderId(orderId);
+    }
 
 }
