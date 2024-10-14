fd53ef48c1206aaa0b9cd8f92f74a7c40e325575
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-14 22:41:25 +0530

    Microservice

---
+++ Spring Cloud Microservices/API-Gateway/src/main/java/com/mylearning/apigateway/config/ApiGatewayConfig.java
@@ -0,0 +1,25 @@
+package com.mylearning.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("order-service", r -> r.path("/orders/**")
+//                        .and()
+//                        .uri("lb://ORDER-MICROSERVICE"))
+//                .route("payment-service", r -> r.path("/payments/**")
+//                        .and()
+//                        .uri("lb://PAYMENT-MICROSERVICE"))
+//                .route("user-service", r -> r.path("/users/**")
+//                        .and()
+//                        .uri("lb://USER-MICROSERVICE"))
+//                .build();
+//    }
+}
