ec95e88f3d25f5a3f028f3d841db55948e89ca3d
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-15 23:28:52 +0530

    Microservice Security

--- Spring Cloud Microservices/API-Gateway/src/main/java/com/mylearning/apigateway/filter/JwtAuthFilter.java
+++ Spring Cloud Microservices/API-Gateway/src/main/java/com/mylearning/apigateway/filter/JwtAuthFilter.java
@@ -1,11 +1,21 @@
 package com.mylearning.apigateway.filter;
 
+import com.mylearning.apigateway.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Config> {
+
+
+    @Autowired
+    private RouteValidator routeValidator;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     public JwtAuthFilter() {
         super(Config.class);
@@ -13,9 +23,29 @@ public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Co
 
     @Override
     public GatewayFilter apply(Config config) {
-        return null;
+        return ((exchange, chain) -> {
+            if (routeValidator.isSecured.test(exchange.getRequest())) {
+
+                //header contains token or not
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("missing authorization header");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+                try {
+                    jwtUtils.validateToken(authHeader);
+                } catch (Exception e) {
+                    System.out.println("invalid access...!");
+                    throw new RuntimeException("un authorized access to application");
+                }
+            }
+            return chain.filter(exchange);
+        });
     }
-    
+
     public static class Config {
 
     }
---
+++ Spring Cloud Microservices/API-Gateway/src/main/java/com/mylearning/apigateway/filter/RouteValidator.java
@@ -0,0 +1,24 @@
+package com.mylearning.apigateway.filter;
+
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    // this request will b e bypassed
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register",
+            "/auth/token",
+            "/eureka"
+    );
+
+    //apart from above any request comes will be authenticated
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
