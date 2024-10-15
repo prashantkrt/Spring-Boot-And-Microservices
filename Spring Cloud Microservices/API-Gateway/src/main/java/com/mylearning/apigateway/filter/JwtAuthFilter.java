d9060b5957f99d76ee366c84de1f94283620f016
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-15 22:36:10 +0530

    Microservice Security

---
+++ Spring Cloud Microservices/API-Gateway/src/main/java/com/mylearning/apigateway/filter/JwtAuthFilter.java
@@ -0,0 +1,23 @@
+package com.mylearning.apigateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Config> {
+
+    public JwtAuthFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return null;
+    }
+    
+    public static class Config {
+
+    }
+
+}
