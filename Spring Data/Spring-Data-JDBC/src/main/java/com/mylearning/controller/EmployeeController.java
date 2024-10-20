cf092e834c0359220b6266f3130cda77c9c51ce7
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-20 22:05:44 +0530

    Spring Data JDBC

--- Spring Data/Spring-Data-JDBC/src/main/java/com/mylearning/controller/EmployeeController.java
+++ Spring Data/Spring-Data-JDBC/src/main/java/com/mylearning/controller/EmployeeController.java
@@ -1,16 +1,57 @@
 package com.mylearning.controller;
 
+import com.mylearning.entity.Employee;
 import com.mylearning.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class EmployeeController {
 
     @Autowired
-    private  EmployeeService employeeService;
+    private EmployeeService employeeService;
 
+    @PostMapping
+    public String saveEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    @GetMapping("/2nd")
+    public List<Employee> getEmployeesBy2ndApproach() {
+        return employeeService.getEmployeesUsingBeanPropertyRowMapper();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @GetMapping("/name/{id}")
+    public String getNameById(@PathVariable int id) {
+        return employeeService.getNameById(id);
+    }
+
+    @GetMapping("/{name}/{dept}")
+    public List<Employee> findEmployeesByNameAndDept(@PathVariable String name, @PathVariable String dept) {
+        return employeeService.findEmployeesByNameAndDept(name, dept);
+    }
+
+    @PutMapping
+    public String updateEmployee(@RequestBody Employee employee) {
+        return employeeService.updateEmployee(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        return employeeService.deleteEmployee(id);
+    }
 
 }
--- Spring Data/Spring-Data-JDBC/src/main/java/com/mylearning/service/EmployeeService.java
+++ Spring Data/Spring-Data-JDBC/src/main/java/com/mylearning/service/EmployeeService.java
@@ -1,12 +1,52 @@
 package com.mylearning.service;
 
+import com.mylearning.entity.Employee;
 import com.mylearning.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    public String saveEmployee(Employee employee) {
+        int count = employeeRepository.save(employee);
+        return "RECORD INSERTED ! " + count;
+    }
+
+    public List<Employee> getEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public List<Employee> getEmployeesUsingBeanPropertyRowMapper(){
+        return employeeRepository.getAllEmployees();
+    }
+
+    public Employee getEmployeeById(int id){
+        return employeeRepository.findById(id);
+    }
+
+
+    public String getNameById(int id){
+        return employeeRepository.getNameById(id);
+    }
+
+    public List<Employee> findEmployeesByNameAndDept(String name,String dept){
+        return employeeRepository.findAllByNameAndDepartment(name, dept);
+    }
+
+
+    public String updateEmployee(Employee employee){
+        int count= employeeRepository.updateEmployee(employee);
+        return count+" RECORD UPDATED !";
+    }
+
+    public String deleteEmployee(int id){
+        int count= employeeRepository.delete(id);
+        return count+" RECORD DELETED !";
+    }
+
 }
