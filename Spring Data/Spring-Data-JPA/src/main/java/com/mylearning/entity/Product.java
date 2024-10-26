77ef29b39d4da030476502c5171252853dbbb740
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-26 22:36:05 +0530

    Spring Boot Data JPA

---
+++ Spring Data/Spring-Data-JPA/src/main/java/com/mylearning/entity/Product.java
@@ -0,0 +1,14 @@
+package com.mylearning.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    private Integer id;
+    private String name;
+    private String descritpion;
+}
