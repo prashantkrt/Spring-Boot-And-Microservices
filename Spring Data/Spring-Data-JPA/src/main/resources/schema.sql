e01cdc2c219a2827c0030b6f7c4253ebb3454538
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-28 11:56:30 +0530

    Spring Boot Data JPA

---
+++ Spring Data/Spring-Data-JPA/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+-- Create Table
+-- Drop Table
+-- Insert Table
+
+insert into PRODUCT_TABLE (name,price,description,productType) values('Iphone 11 Pro',100000,'Apple Product','Electronics');
+
+insert into PRODUCT_TABLE (name,price,description,productType) values('Iphone 13',80000,'Apple Product','Electronics');
+
+insert into PRODUCT_TABLE (name,price,description,productType) values('TV',80000,'Sony Bravia','Electronics');
+
+insert into PRODUCT_TABLE (name,price,description,productType) values('Macbook pro ',169999,'Apple Product','Electronics');
+
+insert into PRODUCT_TABLE (name,price,description,productType) values('Macbook Air',129999,'Apple Product','Electronics');
