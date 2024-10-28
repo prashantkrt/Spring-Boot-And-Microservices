f9ffe24409afdc23ac214e5b3040a5d2572dd642
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-28 12:05:25 +0530

    Spring Boot Data JPA

--- Spring Data/Spring-Data-JPA/src/main/resources/schema.sql
+++ Spring Data/Spring-Data-JPA/src/main/resources/schema.sql
@@ -2,12 +2,9 @@
 -- Drop Table
 -- Insert Table
 
-insert into PRODUCT_TABLE (name,price,description,productType) values('Iphone 11 Pro',100000,'Apple Product','Electronics');
+-- Used for creating database schema (tables, indexes, constraints, etc.).
+-- Ideal when you want to define the structure of your database explicitly instead of relying on automatic schema generation by JPA.
+-- Typically contains CREATE TABLE, ALTER TABLE, and other DDL (Data Definition Language) statements.
 
-insert into PRODUCT_TABLE (name,price,description,productType) values('Iphone 13',80000,'Apple Product','Electronics');
 
-insert into PRODUCT_TABLE (name,price,description,productType) values('TV',80000,'Sony Bravia','Electronics');
-
-insert into PRODUCT_TABLE (name,price,description,productType) values('Macbook pro ',169999,'Apple Product','Electronics');
-
-insert into PRODUCT_TABLE (name,price,description,productType) values('Macbook Air',129999,'Apple Product','Electronics');
+-- Use schema.sql when you need explicit control over the schema or are not relying on Hibernate for schema generation.
