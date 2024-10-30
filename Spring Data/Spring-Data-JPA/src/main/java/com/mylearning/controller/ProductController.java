1211174dac1052633c8101229c323c8fb1158351
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-10-30 22:30:03 +0530

    Spring Boot Data JPA

--- Spring Data/Spring-Data-JPA/src/main/java/com/mylearning/controller/ProductController.java
+++ Spring Data/Spring-Data-JPA/src/main/java/com/mylearning/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.mylearning.controller;
 
 
+import com.mylearning.entity.Product;
 import com.mylearning.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -12,4 +14,24 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id) {
+        return productService.getProductById(id);
+    }
+    @GetMapping("/name/{name}")
+    public List<Product> getProductByName(@PathVariable String name) {
+        return productService.getProductByName(name);
+    }
+
 }
