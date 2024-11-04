1f3e84f42743cf0745640ff8b7f966bb32bc6173
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-04 15:14:38 +0530

    Spring Boot Data MongoDB

--- Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/service/TaskService.java
+++ Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/service/TaskService.java
@@ -1,6 +1,5 @@
 package com.mylearning.springdatamongodb.service;
 
-
 import com.mylearning.springdatamongodb.model.Task;
 import com.mylearning.springdatamongodb.repository.TaskRepository;
 import org.springframework.stereotype.Service;
@@ -28,6 +27,20 @@ public class TaskService {
 
     public Task getTask(String taskId) {
         return taskRepository.findById(taskId).get();
+    }
+
+    public Task updateTask(Task taskRequest) {
+        Task existingTask = taskRepository.findById(taskRequest.getTaskId()).get();// DB data
+        existingTask.setTaskDescription(taskRequest.getTaskDescription());
+        existingTask.setTaskDescription(taskRequest.getTaskDescription());
+        existingTask.setTaskDescription(taskRequest.getTaskDescription());
+        existingTask.setTaskDescription(taskRequest.getTaskDescription());
+        return taskRepository.save(existingTask);
+    }
+
+    public String deleteTask(String taskId) {
+        taskRepository.deleteById(taskId);
+        return "Successfully deleted task " + taskId;
     }
 
 }
