63525f24fa2ba73508c7c7f1db11da1d055fd5dd
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-04 12:00:10 +0530

    Spring Boot Data JPA

--- Spring Data/Spring-Data-JPA/src/main/java/com/mylearning/repository/ProductRepository.java
+++ Spring Data/Spring-Data-JPA/src/main/java/com/mylearning/repository/ProductRepository.java
@@ -6,44 +6,53 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product,Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     // Derived Query Methods
     List<Product> findByName(String name);
+
     List<Product> findByProductType(String productType);
+
     List<Product> findByNameAndProductType(String name, String productType);
+
     List<Product> findByPriceOrDescription(double price, String description);
 
     List<Product> findByDescriptionIsNotNull();
+
     List<Product> findByNameIgnoreCaseContaining(String name);
 
     //prefix + field + operator
     List<Product> findByPriceGreaterThan(double price);
+
     List<Product> findByPriceBetween(double lower, double upper);
+
     List<Product> findByPriceIn(List<Double> prices);
+
     List<Product> findByPriceLessThan(double price);
 
     void deleteByName(String name);
 
-
     //@Query(value = "SELECT * FROM PRODUCT_TBL WHERE price = ?1",nativeQuery = true)
-    @Query("from Product p where p.price= ?1 ") // position-based parameter
-    //@Query("from PRODUCT_TABLE p where p.price= :price") //named parameter base index
+    @Query("from Product p where p.price= ?1 ")
+    // position-based parameter
+    //@Query("from PRODUCT_TABLE p where p.price= :price") // named parameter base index
     List<Product> getProductByPrice(double price);
 
-    // Custom Query Methods
-    @Query("SELECT p FROM Product p WHERE p.price > :price")
+    // Custom Query Methods using JPQL
+    @Query("SELECT p FROM Product p WHERE p.price > :price") // use JPQL in a native query is false else it won't work
     List<Product> findProductsByPriceGreaterThan(double price);
 
-    @Query("SELECT p FROM Product p WHERE p.productType = :productType ORDER BY p.price DESC")
+    @Query("SELECT p FROM Product p WHERE p.productType = :productType ORDER BY p.price DESC") // JPQL
     List<Product> findProductsByProductTypeOrderByPriceDesc(String productType);
 
-    @Query("SELECT p FROM Product p WHERE p.name LIKE %:name%")
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:name%") // JPQL
     List<Product> findProductsByNameContaining(String name);
 
-    @Query(value = "SELECT * FROM Product_Table WHERE product_type = ?1 LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM Product_Table WHERE product_type = ?1 LIMIT 1", nativeQuery = true) // SQL
     Product findFirstByProductType(String productType);
 
+    @Query(value = "SELECT * FROM Product_Table WHERE price < :price", nativeQuery = true) // SQL
+    List<Product> findProductsByPriceLesserThan(double price);
 
 }
 //1. Standard CRUD Methods (Provided by JpaRepository)
