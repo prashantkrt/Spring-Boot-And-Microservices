8951126220976a4463a3bb2064bad40dbf779d67
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-04 15:06:00 +0530

    Spring Boot Data MongoDB

--- Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/model/Task.java
+++ Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/model/Task.java
@@ -4,20 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collation = "Task_Table")
+@Document(collection = "Task")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class Task {
+    @Id
+    @Field("id")
     private String taskId;
+    @Field("name")
     private String taskName;
+    @Field("description")
     private String taskDescription;
+    @Field("status")
     private String taskStatus; //active or inactive
+    @Field("priority")
     private Integer taskPriority; // 1, 2 and 3
+    @Field("type")
     private String taskType;
     private Integer storyPoint;
+    @Field("assignee")
     private String taskAssignee;
 }
--- Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/service/TaskService.java
+++ Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/service/TaskService.java
@@ -1,8 +1,12 @@
 package com.mylearning.springdatamongodb.service;
 
 
+import com.mylearning.springdatamongodb.model.Task;
 import com.mylearning.springdatamongodb.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TaskService {
@@ -11,6 +15,19 @@ public class TaskService {
 
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+    }
+
+    public Task save(Task task) {
+        task.setTaskId(UUID.randomUUID().toString().split("-")[0]);
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTask(String taskId) {
+        return taskRepository.findById(taskId).get();
     }
 
 }
