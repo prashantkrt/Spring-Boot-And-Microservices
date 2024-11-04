f602b437d43507145da4000539738ab883d22c08
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-04 14:52:01 +0530

    Spring Boot Data MongoDB

---
+++ Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/entity/Task.java
@@ -0,0 +1,24 @@
+package com.mylearning.springdatamongodb.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "Task_Table")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Task {
+    private String taskId;
+    private String taskName;
+    private String taskDescription;
+    private String taskStatus; //active or inactive
+    private Integer taskPriority; // 1, 2 and 3
+    private String taskType;
+    private Integer storyPoint;
+    private String taskAssignee;
+}
