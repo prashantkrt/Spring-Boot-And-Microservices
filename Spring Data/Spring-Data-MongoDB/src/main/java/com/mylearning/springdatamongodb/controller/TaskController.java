cb080a1190515747dfaab631061ba63717e91eb9
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-04 15:29:04 +0530

    Spring Boot Data MongoDB

--- Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/controller/TaskController.java
+++ Spring Data/Spring-Data-MongoDB/src/main/java/com/mylearning/springdatamongodb/controller/TaskController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
