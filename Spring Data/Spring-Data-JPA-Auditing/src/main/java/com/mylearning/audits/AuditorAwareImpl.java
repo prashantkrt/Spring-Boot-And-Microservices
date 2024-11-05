f215c66733ca5384ad44686bb4c8546e34ecaf0a
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-05 13:31:39 +0530

    Spring Boot Data Auditing

--- Spring Data/Spring-Data-JPA-Auditing/src/main/java/com/mylearning/audits/AuditorAwareImpl.java
+++ Spring Data/Spring-Data-JPA-Auditing/src/main/java/com/mylearning/audits/AuditorAwareImpl.java
@@ -1,11 +1,9 @@
 package com.mylearning.audits;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.stereotype.Component;
-
 import java.util.Optional;
 
-@Component
+
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
--- Spring Data/Spring-Data-JPA-Auditing/src/main/resources/data.sql
+++ Spring Data/Spring-Data-JPA-Auditing/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- Inserting sample products into the Product table
-INSERT INTO Product_Table (name, price, description, productType)
+INSERT INTO Product_Details(name, price, description, productType)
 VALUES ('Product A', 100.0, 'Description for Product A', 'Type1'),
        ('Product B', 150.5, 'Description for Product B', 'Type2'),
        ('Product C', 200.0, 'Description for Product C', 'Type1'),
