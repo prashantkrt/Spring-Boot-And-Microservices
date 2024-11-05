9177ea89b0125ecb9c90f3beccc7fca6577fca21
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-05 13:22:54 +0530

    Spring Boot Data Auditing

---
+++ Spring Data/Spring-Data-JPA-Auditing/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- Inserting sample products into the Product table
+INSERT INTO Product_Table (name, price, description, productType)
+VALUES ('Product A', 100.0, 'Description for Product A', 'Type1'),
+       ('Product B', 150.5, 'Description for Product B', 'Type2'),
+       ('Product C', 200.0, 'Description for Product C', 'Type1'),
+       ('Product D', 99.9, 'Description for Product D', 'Type3');
