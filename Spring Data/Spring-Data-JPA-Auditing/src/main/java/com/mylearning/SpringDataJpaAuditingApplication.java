313da7a439a34705263f6e3af901c2bf7fc84043
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-05 12:19:33 +0530

    Spring Boot Data Auditing

---
+++ Spring Data/Spring-Data-JPA-Auditing/src/main/java/com/mylearning/SpringDataJpaAuditingApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaAuditingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaAuditingApplication.class, args);
+    }
+
+}
