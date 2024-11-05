3a71d05c643ebe5bf2681c426ff9eb2587508664
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-05 12:42:07 +0530

    Spring Boot Data Auditing

---
+++ Spring Data/Spring-Data-JPA-Auditing/src/main/java/com/mylearning/entity/Product.java
@@ -0,0 +1,22 @@
+package com.mylearning.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="Product_Details")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private String description;
+    private String productType;
+}
---
+++ Spring Data/Spring-Data-JPA-Auditing/src/main/java/com/mylearning/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.mylearning.service;
+
+
+import com.mylearning.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+}
