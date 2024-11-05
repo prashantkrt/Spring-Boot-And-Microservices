11c73c6690a2210df63b3108d954c8f0072ecfae
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-05 22:10:47 +0530

    Spring Boot Data Password Encoder

--- Spring Data/Spring-Data-Jasypt/src/main/java/com/mylearning/config/AuditorAwareConfig.java
+++ Spring Data/Spring-Data-Jasypt/src/main/java/com/mylearning/config/AuditorAwareConfig.java
@@ -1,6 +1,9 @@
 package com.mylearning.config;
 
 import com.mylearning.audits.AuditorAwareImpl;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -12,4 +15,21 @@ public class AuditorAwareConfig {
     public AuditorAware<String> getAuditorAware() {
         return new AuditorAwareImpl();
     }
+
+    //our own encryptor  optional
+//    @Bean(name="jasptyStringEncryptor") // same should be this only in order to use this bean
+//    public StringEncryptor getStringEncryptor() {
+//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+//        config.setPassword("myKey");// private -key
+//        config.setAlgorithm("PBEWithMD5AndDES");
+//        config.setKeyObtentionIterations(1000);
+//        config.setPoolSize(1);
+//        config.setProviderName("SunJCE");
+//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+//        config.setStringOutputType("base64");
+//        encryptor.setConfig(config);
+//        return encryptor;
+//    }
+
 }
