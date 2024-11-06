93ce1760c046fe2db2b5fefc23e928b78ef17a23
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-06 13:54:44 +0530

    Spring Boot JPA Join Mapping

---
+++ Spring Data/Spring-Boot-Mappings/Spring-Boot-Mapping-OneToOne/src/main/java/com/mylearning/springbootmappingonetoone/SpringBootMappingOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.springbootmappingonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMappingOneToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMappingOneToOneApplication.class, args);
+    }
+
+}
