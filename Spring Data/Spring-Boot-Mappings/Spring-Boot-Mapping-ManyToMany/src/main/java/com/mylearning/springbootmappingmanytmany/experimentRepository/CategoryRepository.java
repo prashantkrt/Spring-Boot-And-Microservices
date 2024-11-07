635ee029599283674e1dd4dcefed1c5d4c10a4cc
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-08 00:53:58 +0530

    Spring Boot JPA Join Mapping

--- Spring Data/Spring-Boot-Mappings/Spring-Boot-Mapping-ManyToMany/src/main/java/com/mylearning/springbootmappingmanytmany/experimentRepository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.mylearning.springbootmappingmanytmany.experimentRepository;
-//
-//import com.mylearning.springbootmappingmanytmany.experiment.Category;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CategoryRepository extends JpaRepository<Category, Integer> {
-//}
