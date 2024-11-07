677f9db9bda8c8d3b69408f5fa47269bcb39ea46
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-08 00:48:15 +0530

    Spring Boot JPA Join Mapping

--- Spring Data/Spring-Boot-Mappings/Spring-Boot-Mapping-ManyToMany/src/main/java/com/mylearning/springbootmappingmanytmany/entity/Course.java
+++ Spring Data/Spring-Boot-Mappings/Spring-Boot-Mapping-ManyToMany/src/main/java/com/mylearning/springbootmappingmanytmany/entity/Course.java
@@ -2,6 +2,7 @@ package com.mylearning.springbootmappingmanytmany.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
@@ -11,15 +12,18 @@ import java.util.List;
  *  A Student can enroll in many Courses.
  *  A Course can have many Students.
  * */
-@Entity
+@Entity(name = "Course_TBL")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String courseName;
+    private String teacherName;
+    private int modules;
+    private double fees;
 
     @ManyToMany(mappedBy = "courses") // Inverse side, referring to the courses field in Student
     private List<Student> students = new ArrayList<>();
--- Spring Data/Spring-Boot-Mappings/Spring-Boot-Mapping-ManyToMany/src/main/java/com/mylearning/springbootmappingmanytmany/entity/Student.java
+++ Spring Data/Spring-Boot-Mappings/Spring-Boot-Mapping-ManyToMany/src/main/java/com/mylearning/springbootmappingmanytmany/entity/Student.java
@@ -1,27 +1,36 @@
 package com.mylearning.springbootmappingmanytmany.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 import java.util.ArrayList;
 /*
 *  A Student can enroll in many Courses.
 *  A Course can have many Students.
 * */
-@Entity
+
+@Entity(name="STUDENT_TBL")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
+    private int age;
+    private String department;
 
-    @ManyToMany
+    //owner
+    @ManyToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinTable(
             name = "student_course", // Name of the join table
-            joinColumns = @JoinColumn(name = "student_id"), // Foreign key for Student in the join table
-            inverseJoinColumns = @JoinColumn(name = "course_id") // Foreign key for Course in the join table
+            joinColumns = @JoinColumn(name = "student_id",referencedColumnName = "id"), // Foreign key for Student in the join table
+            inverseJoinColumns = @JoinColumn(name = "course_id",referencedColumnName = "id") // Foreign key for Course in the join table
     )
-    private List<Course> courses = new ArrayList<>();
-
-    // Getters and setters
+    private List<Course> courses;
 }
