85f8d7f38f5867e53f41c89c88853b9f5534ff3a
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-08 14:31:31 +0530

    Spring Boot JPA Transaction

---
+++ Spring Data/Spring-Boot-Transactions/src/test/java/com/mylearning/springboottransactions/SpringBootTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.springboottransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
