91dd216dae8e8693c05df954097dc4b9312e0cb7
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-10 22:46:23 +0530

    Spring Boot Redis

--- Spring Cache/Spring-Boot-Data-Redis-2/src/main/java/com/mylearning/springbootdataredis/hash/Product.java
+++ Spring Cache/Spring-Boot-Data-Redis-2/src/main/java/com/mylearning/springbootdataredis/hash/Product.java
@@ -4,12 +4,14 @@ package com.mylearning.springbootdataredis.hash;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RedisHash("Product") // is not mandatory
 public class Product implements Serializable {
 
     private static final long serialVersionUID = 1L;
