0fe315a15f2e9a9eccb5b04857a4c1f35eeb9935
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-10 18:09:21 +0530

    Spring Boot Cache

--- Spring Cache/Spring-Boot-Cache/src/main/java/com/mylearning/springbootcache/service/ProductService.java
+++ Spring Cache/Spring-Boot-Cache/src/main/java/com/mylearning/springbootcache/service/ProductService.java
@@ -2,6 +2,7 @@ package com.mylearning.springbootcache.service;
 
 import com.mylearning.springbootcache.entity.Product;
 import com.mylearning.springbootcache.repository.ProductRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -21,17 +22,20 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public void initDB() {
-        //new Random().nextInt(5000): Generates a random integer in the range from 0 (inclusive) up to but not including 5000.
-        //The resulting random integer will be in the range 0 to 4999.
-        productRepository.saveAll(IntStream.rangeClosed(1, 10000).mapToObj(i -> new Product("Product" + i, new Random().nextInt(5000), "Product Description => " + i, "product type => " + i))
-                .collect(Collectors.toList()));
-    }
+//   Loading the data first time while starting the application
+//    @PostConstruct
+//    public void initDB() {
+//        //new Random().nextInt(5000): Generates a random integer in the range from 0 (inclusive) up to but not including 5000.
+//        //The resulting random integer will be in the range 0 to 4999.
+//        productRepository.saveAll(IntStream.rangeClosed(1, 10000).mapToObj(i -> new Product("Product" + i, new Random().nextInt(5000), "Product Description => " + i, "product type => " + i))
+//                .collect(Collectors.toList()));
+//    }
 
     public Product saveProduct(Product product) {
         return productRepository.save(product);
     }
 
+    // will reduce the fetch timing due to caching
     @Cacheable(cacheNames = "productsCache")
     public List<Product> getProducts() {
         log.info("Inside DB product service => getProducts");
