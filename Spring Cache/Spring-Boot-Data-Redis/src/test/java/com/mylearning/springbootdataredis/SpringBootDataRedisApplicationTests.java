f2c31e38d397c070636515ccdca7e9bbe6c4fdc8
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-10 19:30:53 +0530

    Spring Boot Redis

---
+++ Spring Cache/Spring-Boot-Data-Redis/src/test/java/com/mylearning/springbootdataredis/SpringBootDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.springbootdataredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
