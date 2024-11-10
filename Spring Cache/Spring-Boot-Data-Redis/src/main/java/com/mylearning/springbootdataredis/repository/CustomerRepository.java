67b343cd3bdf5d237d075d6cd59c021832d08a8e
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-10 22:26:06 +0530

    Spring Boot Redis

--- Spring Cache/Spring-Boot-Data-Redis/src/main/java/com/mylearning/springbootdataredis/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mylearning.springbootdataredis.repository;
-
-import com.mylearning.springbootdataredis.hash.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-// no need
-//public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-//}
