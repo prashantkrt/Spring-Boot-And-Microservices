40049f11c32af74fa8af9eceb72de7934b76e085
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-14 21:13:43 +0530

    Spring Boot AOP

--- AOP-Aspect Orinted Programming/Spring-Boot-AOP/src/main/java/com/mylearning/advice/LoggingAdvice.java
+++ AOP-Aspect Orinted Programming/Spring-Boot-AOP/src/main/java/com/mylearning/advice/LoggingAdvice.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -39,5 +40,23 @@ public class LoggingAdvice {
     public void logResponseAfterReturn(JoinPoint joinPoint) throws JsonProcessingException {
         log.info("LoggingAdvice::logResponse class name {} ,method name {} after method completion", joinPoint.getTarget(), joinPoint.getSignature().getName());
         log.info("LoggingAdvice::logResponse Response Body {} after method completion ", new ObjectMapper().writeValueAsString(joinPoint.getArgs()));
+    }
+
+    @Around("execution(* com.mylearning.service.ProductService.getProductById())")
+    public Object captureRequestAndResponse(ProceedingJoinPoint pjp) throws Throwable {
+
+        //before logic
+        System.out.println("=====================BEFORE START==========================================");
+        log.info("execution started {} ", pjp.getSignature());
+        log.info("Request body {}", new ObjectMapper().writeValueAsString(pjp.getArgs()));
+        System.out.println("=====================BEFORE END============================================");
+        Object obj = pjp.proceed();
+
+        //after logic
+        System.out.println("=====================AFTER START===========================================");
+        log.info("execution ended {} ", pjp.getSignature());
+        log.info("Response  body {}", new ObjectMapper().writeValueAsString(obj));
+        System.out.println("=====================AFTER END=============================================");
+        return obj;
     }
 }
