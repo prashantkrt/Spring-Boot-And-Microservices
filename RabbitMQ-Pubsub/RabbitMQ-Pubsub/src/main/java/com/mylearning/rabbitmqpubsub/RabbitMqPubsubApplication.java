bb2303b2889586987d4eb576f16877edf3e4759e
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-20 11:22:44 +0530

    RabbitMQ

---
+++ RabbitMQ-Pubsub/RabbitMQ-Pubsub/src/main/java/com/mylearning/rabbitmqpubsub/RabbitMqPubsubApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.rabbitmqpubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqPubsubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqPubsubApplication.class, args);
+    }
+
+}
---
+++ RabbitMQ-Pubsub/RabbitMQ-Pubsub/src/test/java/com/mylearning/rabbitmqpubsub/RabbitMqPubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.rabbitmqpubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqPubsubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
