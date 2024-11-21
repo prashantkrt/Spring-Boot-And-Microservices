477632b84ea60d065a9ddb46601257597830a67e
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-21 21:58:05 +0530

    Spring Boot Batch

--- Spring-Boot-Batch/Spring-Boot-Batch/src/main/java/com/mylearning/springbootbatch/config/ApplicationBatchConfiguration.java
+++ Spring-Boot-Batch/Spring-Boot-Batch/src/main/java/com/mylearning/springbootbatch/config/ApplicationBatchConfiguration.java
@@ -3,7 +3,12 @@ package com.mylearning.springbootbatch.config;
 
 import com.mylearning.springbootbatch.entity.Customer;
 import com.mylearning.springbootbatch.repository.CustomerRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
@@ -14,7 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
@@ -23,9 +28,16 @@ public class ApplicationBatchConfiguration {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+
     // Item Reader
     @Bean
-    public FlatFileItemReader<Customer> customerFlatFileItemReader() {
+    public FlatFileItemReader<Customer> customerItemReader() {
 
         FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
 
@@ -46,7 +58,7 @@ public class ApplicationBatchConfiguration {
         DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
         lineTokenizer.setDelimiter(",");
         lineTokenizer.setStrict(false);
-        lineTokenizer.setNames("customerId", "firstName","lastName","email","gender","contactNo","country","dateOfBirth");
+        lineTokenizer.setNames("customerId", "firstName", "lastName", "email", "gender", "contactNo", "country", "dateOfBirth");
 
         //set it to type customer type
         BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
@@ -64,8 +76,9 @@ public class ApplicationBatchConfiguration {
         return new CustomerItemProcessor();
     }
 
-    //Item Writer
-    public RepositoryItemWriter<Customer> customerRepositoryItemWriter() {
+    // Item Writer
+    @Bean
+    public RepositoryItemWriter<Customer> customerItemWriter() {
         RepositoryItemWriter<Customer> itemWriter = new RepositoryItemWriter<>();
         itemWriter.setRepository(customerRepository);
         itemWriter.setMethodName("save");
@@ -73,4 +86,36 @@ public class ApplicationBatchConfiguration {
     }
 
 
+    @Bean
+    public Step customerStep() {
+        return new StepBuilder("step-1", jobRepository)
+                .<Customer, Customer>chunk(10, transactionManager) //a chunk is a way to process a large volume of data efficiently and transactionally by dividing it into smaller, manageable pieces (or "chunks").
+                .reader(customerItemReader())
+                .processor(customerItemProcessor())
+                .writer(customerItemWriter())
+                .build();
+
+//        Here, the chunk(10) means:
+//        10 items will be read, processed, and written together.
+//        A single transaction will encompass these 10 items.
+//        If any of these 10 items fail at any stage, the entire chunk is rolled back.
+//
+//        ACID Property
+//        If one record in the chunk causes a failure (e.g., due to a database constraint), the transaction ensures that:
+//          => All operations in the chunk are rolled back.
+//          => No partial writes occur.
+//          => The job can restart from the last successful chunk.
+//        The TransactionManager in Spring Batch is a key component responsible for managing transactions during the execution of a batch step.
+//        It ensures that the operations within a chunk (such as reading, processing, and writing data) are executed in a transactional context.
+
+    }
+
+    @Bean
+    public Job job() {
+        ////return new JobBuilder("customers-import", jobRepository).start(step()).next(step2()).next(step3()).build();
+        return new JobBuilder("customers-import", jobRepository)
+                .flow(customerStep())
+                .end()
+                .build();
+    }
 }
