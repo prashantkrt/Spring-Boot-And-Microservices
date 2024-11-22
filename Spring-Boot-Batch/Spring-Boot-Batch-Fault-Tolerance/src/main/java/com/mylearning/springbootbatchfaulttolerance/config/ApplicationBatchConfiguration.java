34d56d5dbcbb4aee7768ab54992e6605cb1faf70
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-11-22 14:29:59 +0530

    Spring Boot Batch Fault Tolerance

--- Spring-Boot-Batch/Spring-Boot-Batch-Fault-Tolerance/src/main/java/com/mylearning/springbootbatchfaulttolerance/config/ApplicationBatchConfiguration.java
+++ Spring-Boot-Batch/Spring-Boot-Batch-Fault-Tolerance/src/main/java/com/mylearning/springbootbatchfaulttolerance/config/ApplicationBatchConfiguration.java
@@ -85,40 +85,10 @@ public class ApplicationBatchConfiguration {
         return itemWriter;
     }
 
-
-    // Step
-    @Bean
-    public Step customerStep() {
-        return new StepBuilder("step-1", jobRepository)
-                .<Customer, Customer>chunk(10, transactionManager) //a chunk is a way to process a large volume of data efficiently and transactionally by dividing it into smaller, manageable pieces (or "chunks").
-                .reader(customerItemReader())
-                .processor(customerItemProcessor())
-                .writer(customerItemWriter())
-                .build();
-
-//        Here, the chunk(10) means:
-//        10 items will be read, processed, and written together.
-//        A single transaction will encompass these 10 items.
-//        If any of these 10 items fail at any stage, the entire chunk is rolled back.
-//
-//        ACID Property
-//        If one record in the chunk causes a failure (e.g., due to a database constraint), the transaction ensures that:
-//          => All operations in the chunk are rolled back.
-//          => No partial writes occur.
-//          => The job can restart from the last successful chunk.
-//        The TransactionManager in Spring Batch is a key component responsible for managing transactions during the execution of a batch step.
-//        It ensures that the operations within a chunk (such as reading, processing, and writing data) are executed in a transactional context.
-
-    }
-
-    // better understanding with params
-    // lets create a step-2
-    @Bean(name="step2")
-    public Step customerStep2(ItemReader<Customer> reader,
+    @Bean(name = "customerStep")
+    public Step customerStep(ItemReader<Customer> reader,
                              ItemProcessor<Customer, Customer> processor,
-                             ItemWriter<Customer> writer,
-                             JobRepository jobRepository,
-                             PlatformTransactionManager transactionManager) {
+                             ItemWriter<Customer> writer) {
         return new StepBuilder("step-2", jobRepository)
                 .<Customer, Customer>chunk(10, transactionManager)  // The chunk size is 10, transaction manager is used for transactional processing
                 .reader(reader)                                    // The ItemReader to read data
@@ -130,10 +100,9 @@ public class ApplicationBatchConfiguration {
 
     //Job
     @Bean
-    public Job job() {
-        //return new JobBuilder("customers-import", jobRepository).start(step()).next(step2()).next(step3()).build();
+    public Job job(Step customerStep) {
         return new JobBuilder("customers-import", jobRepository)
-                .flow(customerStep())
+                .flow(customerStep)
                 .end()
                 .build();
     }
